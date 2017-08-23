79c9a4f01a814a8a88d89f88365ed02da178020a
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-08-23 16:17:02 +0200

    consumer messages
    - producer started on localhost, outside python (proof of connectivity)

---
+++ kafkaConsumer.py
@@ -0,0 +1,9 @@
+from kafka import KafkaConsumer
+
+try:
+    consumer = KafkaConsumer('aptop',bootstrap_servers=['localhost:9092'])
+    for msg in consumer:
+        print('Message from Consumer: '+ str(msg))
+    KafkaConsumer(consumer_timeout_ms=10000) #10 seconds
+except Exception as e:
+    print("Leider exception in Kafka: "+ e.message)
