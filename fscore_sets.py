6fa53e0ac045e112eeb222cb422a5a3881facc0b
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-09-22 17:30:46 +0200

    Update fscore_sets.py

--- fscore_sets.py
+++ fscore_sets.py
@@ -4,14 +4,19 @@ setC = [(1,2,50,70),(11,13,12,15)]
 setD = [(10,20,50,63,70),(0,2,7,1,4)]
 
 
-operation = tf.sets.set_intersection(setC,setD)
+ooperation = tf.sets.set_intersection(setC,setD)
 operation_run = sess.run(operation)
-print ("Intersection of "+str(setC)+" and "+ str(setD)+" is at \n")
-print((operation_run))
+print ("Intersection of "+str(setC)+" and "+ str(setD)+" is at ",operation_run.values)
 
-dense = tf.sparse_tensor_to_dense(operation_run)
-print("\n")
-print(sess.run(dense))
+for m in operation_run.values:
+    lcount+=1
+    #cannot calculate fscore without index
+
+print("Lcount is ",lcount," and Fscore is ",fscore)
+
+#dense = tf.sparse_tensor_to_dense(operation_run)
+#print("\n")
+#print(sess.run(dense))
 
 ### OUTPUT:
 ### Intersection of [(1, 2, 50, 70), (11, 13, 12, 15)] and [(10, 20, 50, 63, 70), (0, 2, 7, 1, 4)] is at 
@@ -21,5 +26,4 @@ print(sess.run(dense))
 ###                     dense_shape=array([2, 2], dtype=int64))
 
 
-### [[50 70]
-###  [ 0  0]]
+### Lcount is  2  and Fscore is  0.0
