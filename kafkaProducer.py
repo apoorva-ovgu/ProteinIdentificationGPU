e125165098c194f7e6b0df762fdef9e0056e86b0
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-10-03 13:26:40 +0200

    architecture update

--- kafkaProducer.py
+++ kafkaProducer.py
@@ -1,10 +1,20 @@
+import os
 from kafka import KafkaProducer
+
+mgf_location = os.path.join(os.path.dirname(__file__), 'datafiles')
 
 try:
     producer = KafkaProducer(bootstrap_servers=['localhost: 9092'])
-    future = producer.send('aptop', b'raw_bytes')
-    for i in range(1,1000):
-        producer.send('aptop', "Sending "+ str(i))
+    #Send to topic, and send raw bytes
+    future = producer.send('xtandemtest', b'raw_bytes')
+
+    for file in os.listdir(mgf_location):
+        if file.endswith(".mgf"):
+            for line in open(mgf_location + "/" + file, 'U'):
+                line.rstrip('\n')
+
+                producer.send("xtandemtest",line)
+                print("sent "+line)
 
 except Exception as e:
     print("Leider exception in Kafka producer: "+ e.message)
