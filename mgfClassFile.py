82560479855653aaa05863f976bba651ed7f6870
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-12-12 17:46:45 +0100

    Update mgfClassFile.py

--- mgfClassFile.py
+++ mgfClassFile.py
@@ -3,6 +3,6 @@ class mgfClass:
     def __init__(self, name, match, score, remainingComparisons, timeReqd):
         self.name = name
         self.match = match
-        self.score = score
+        self.score = float(score)
         self.remainingComparisons = remainingComparisons
         self.timeReqd = timeReqd
